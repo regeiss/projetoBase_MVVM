0cf61e4aa02e262f33e8906325169c65f8220e4a
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2022-04-07 22:48:33 -0300

    Initial Commit

---
+++ projetoBase_MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  projetoBase_MVVM
+//
+//  Created by Roberto Edgar Geiss on 07/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projetoBase_MVVM/projetoBase_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetoBase_MVVMApp.swift
+//  projetoBase_MVVM
+//
+//  Created by Roberto Edgar Geiss on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct projetoBase_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
