9ba86cb71dab95963e1d59604f1c4a767cbb529e
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2022-04-19 20:56:29 -0300

    incluido arquivo para endpoints

---
+++ projetoBase_MVVM/Base/Constants/QuotesEndPoint.swift
@@ -0,0 +1,52 @@
+//
+//  QuotesEndPoint.swift
+//  projetoBase_MVVM
+//
+//  Created by Roberto Edgar Geiss on 19/04/22.
+//
+
+// TODO: ajustar os endpoints 19/04/2022 15:32:03
+import Foundation
+
+protocol APIBuilder
+{
+    var urlRequest: URLRequest { get }
+    var baseUrl: URL { get }
+    var path: String { get }
+}
+
+enum QuoteAPI
+{
+    case getPosts
+}
+
+extension QuoteAPI: APIBuilder
+{
+
+    var baseUrl: URL
+    {
+        switch self
+        {
+        case .getPosts:
+            return URL(string: "https://api.lil.software")!
+        }
+    }
+    
+    var path: String
+    {
+        switch self
+        {
+            case .getPosts:
+            return "/news"
+        }
+    }
+    
+    var urlRequest: URLRequest
+    {
+        switch self
+        {
+            case .getPosts:
+            return URLRequest(url: self.baseUrl.appendingPathComponent(self.path))
+        }
+    }
+}
