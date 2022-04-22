562befa79b812b7ccc199504bd177d77180e1ae7
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-04-22 16:33:18 -0300

    Create studySnippets.swift

---
+++ projetoBase_MVVM/studySnippets.swift
@@ -0,0 +1,39 @@
+//
+// pull para refresh
+
+struct NewsItem: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let strap: String
+}
+
+struct ContentView: View {
+    @State private var news = [
+        NewsItem(id: 0, title: "Want the latest news?", strap: "Pull to refresh!")
+    ]
+
+    var body: some View {
+        NavigationView {
+            List(news) { item in
+                VStack(alignment: .leading) {
+                    Text(item.title)
+                        .font(.headline)
+                    Text(item.strap)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .refreshable {
+                do {
+                    // Fetch and decode JSON into news items
+
+                    let url = URL(string: "https://www.hackingwithswift.com/samples/news-1.json")!
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    news = try JSONDecoder().decode([NewsItem].self, from: data)
+                } catch {
+                    // Something went wrong; clear the news
+                    news = []
+                }
+            }
+        }
+    }
+}
