c699a3eef380201a44cd249675910b899838d750
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-06-09 13:52:27 -0300

    Update studySnippets.swift
    incluido ajuste para search na view

--- projetoBase_MVVM/studySnippets.swift
+++ projetoBase_MVVM/studySnippets.swift
@@ -37,3 +37,45 @@ struct ContentView: View {
         }
     }
 }
+
+//====================================================================================================
+searchable - Paul Hudson
+struct ContentView: View {
+    let names = ["Holly", "Josh", "Rhonda", "Ted"]
+    @State private var searchText = ""
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(searchResults, id: \.self) { name in
+                    NavigationLink(destination: Text(name)) {
+                        Text(name)
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Contacts")
+        }
+    }
+
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return names
+        } else {
+            return names.filter { $0.contains(searchText) }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
